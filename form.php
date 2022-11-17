2efba30bf8fa7afe8a0bbf8b9c9506770dd1987a
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2022-11-17 16:20:41 +0500

    initial commit

--- form.php
+++ form.php
@@ -1,16 +1,16 @@
 <?php
-    if(isset($_POST['submit'])){
-        $name=$_POST['name'];
-        $email=$_POST['email'];
-        $massage=$_POST['massage'];
+    if(!empty($_POST["send"])){
+        $name=$_POST["name"];
+        $email=$_POST["email"];
+        $massage=$_POST["massage"];
 
-        $to='farihaiqbal015@gmail.com';
-        $subject='form';
-        $message="name : ".$name."\n"."Massage : ".$massage.
-        $headers="From: ".$email;
-        
-        if(mail($to,$subject,$message,$headers)){
-            header(': index.html');
+        $to="farihaiqbal015@gmail.com";
+        $mailHeaders="Name : ". $name .
+        "\r\n Email: ". $email .
+        "\r\n Massage: ". $massage . "\r\n";
+
+        if(mail($to,$name,$mailHeaders)){
+            $messege="successfully";
         }
     
 
