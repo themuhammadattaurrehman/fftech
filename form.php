8a0a65e8dcbfe5933e2a9dac957bb1de5f8b2df5
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2022-11-18 13:01:01 +0500

    Update form.php

--- form.php
+++ form.php
@@ -2,18 +2,18 @@
     if(!empty($_POST["send"])){
         $name=$_POST["name"];
         $email=$_POST["email"];
+        $phone=$_POST['phone'];
         $massage=$_POST["massage"];
 
         $to="farihaiqbal015@gmail.com";
         $mailHeaders="Name : ". $name .
         "\r\n Email: ". $email .
+        "\r\n phone: ". $phone .
         "\r\n Massage: ". $massage . "\r\n";
 
         if(mail($to,$name,$mailHeaders)){
             $messege="successfully";
         }
-    
-
 
     }
 ?>
